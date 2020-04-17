c64da78255d58797fc6bfee0a2c96f23d8f8a6bf
benjmhart <benjmhart@gmail.com>
2020-04-17 18:08:42 -0400

    add object validation tests

--- test/validator-quickCheck/Validator.purs
+++ test/validator-quickCheck/Validator.purs
@@ -5,11 +5,13 @@ import Control.Monad.Except (runExcept)
 import Control.Monad.Trans.Class (lift)
 import Data.Argonaut (Json)
 import Data.Argonaut as Arg
-import Data.Argonaut.Arbitrary (RandomJson(..))
+import Data.Argonaut.Arbitrary (RandomJson(..), ObjGenOpts(..), arbitraryObj)
 import Data.Medea (validate)
-import Data.Medea.Loader (loadSchemaFromFile)
+import Data.Medea.Loader (loadSchemaFromFile, LoaderError)
 import Data.Medea.Schema (Schema)
 import Data.NonEmpty (NonEmpty, (:|))
+import Effect (Effect)
+import Foreign.Object as Obj
 import Mote (group, test)
 import Test.QuickCheck.Combinators ((==>))
 import Test.QuickCheck (Result, arbitrary, quickCheck, quickCheckGen, withHelp, (<?>))
@@ -24,90 +26,222 @@ import Effect.Class (liftEffect)
 suite :: TestPlanM Unit
 suite = do
   testAny 
-    "./conformance/validation/any.medea" "Any Schema"
+    "any.medea" "Any Schema"
   testSingular 
-    "./conformance/validation/null.medea" "Null Schema" Arg.isNull
+    "null.medea" "Null Schema" Arg.isNull
   testSingular 
-    "./conformance/validation/boolean.medea" "Boolean Schema" Arg.isBoolean
+    "boolean.medea" "Boolean Schema" Arg.isBoolean
   testSingular 
-    "./conformance/validation/number.medea" "Number Schema" Arg.isNumber
+    "number.medea" "Number Schema" Arg.isNumber
   testSingular 
-    "./conformance/validation/string.medea" "String Schema" Arg.isString
+    "string.medea" "String Schema" Arg.isString
   testSingular 
-    "./conformance/validation/array.medea" "Array Schema" Arg.isArray
+    "array.medea" "Array Schema" Arg.isArray
   testSingular 
-    "./conformance/validation/object.medea" "Object Schema" Arg.isObject
+    "object.medea" "Object Schema" Arg.isObject
   testSingular 
-    "./conformance/validation/nullable-boolean.medea" 
+    "nullable-boolean.medea" 
     "Boolean/null Schema" 
     (isNullOr Arg.isBoolean)
   testSingular 
-    "./conformance/validation/nullable-number.medea" 
+    "nullable-number.medea" 
     "Number Schema" 
     (isNullOr Arg.isNumber)
   testSingular 
-    "./conformance/validation/nullable-string.medea" 
+    "nullable-string.medea" 
     "String Schema" 
     (isNullOr Arg.isString)
   testSingular 
-    "./conformance/validation/nullable-array.medea" 
+    "nullable-array.medea" 
     "Array Schema" 
     (isNullOr Arg.isArray)
   testSingular 
-    "./conformance/validation/nullable-object.medea" 
+    "nullable-object.medea" 
     "Object Schema" 
     (isNullOr Arg.isObject)
   testStringVals
-    "./conformance/validation/stringVals.medea"
+    "stringVals.medea"
     ("bar" :| ["baz"])
   testStringVals
-    "./conformance/validation/stringVals2.medea"
-    ("accountant" :| ["barber", "bishop", "baker"])
+    "stringVals2.medea"
+    ("accountant" :| ["barber", "bishop", "baker"])  
+    -- Tests for object property checks.
+  testObject
+    (arbitraryObj $ ObjGenOpts ["foo"] [] 0 0)
+    "Object schema with 1 property"
+    "1-property-no-additional-1.medea"
+    (hasProperty "foo" Arg.isBoolean)
+  testObject 
+    (arbitraryObj $ ObjGenOpts ["foo"] [] 0 0)
+    "Object schema with 1 property"
+    "1-property-no-additional-2.medea"
+    (hasProperty "foo" Arg.isNull)
+  testObject 
+    (arbitraryObj $ ObjGenOpts [] ["foo"] 0 0)
+    "Object schema with 1 property"
+    "1-property-no-additional-3.medea"
+    (hasOptionalProperty "foo" Arg.isArray)
+  testObject 
+    (arbitraryObj $ ObjGenOpts ["foo"] [] 0 3)
+    "Object schema with 1 property and additional allowed"
+    "1-property-additional-1.medea"
+    (hasProperty "foo" Arg.isString)
+  testObject
+    (arbitraryObj $ ObjGenOpts ["foo"] [] 0 3)
+    "Object schema with 1 property and additional allowed"
+    "1-property-additional-2.medea"
+    (hasProperty "foo" Arg.isNumber)
+  testObject
+    (arbitraryObj $ ObjGenOpts ["foo"] [] 0 3)
+    "Object schema with 1 property and additional allowed"
+    "1-property-additional-3.medea"
+    (hasOptionalProperty "foo" Arg.isObject)
+  testObject
+    (arbitraryObj $ ObjGenOpts ["foo", "bar", "bazz"] [] 0 0)
+    "Object schema with 3 properties"
+    "3-property-no-additional-1.medea"
+    (\x -> hasProperty "foo" Arg.isBoolean x && hasProperty "bazz" Arg.isString x)
+  testObject
+    (arbitraryObj $ ObjGenOpts ["bar", "bazz"] ["foo"] 0 0)
+    "Object schema with 3 properties"
+     "3-property-no-additional-2.medea"
+    (\x -> hasOptionalProperty "foo" Arg.isNumber x && hasProperty "bazz" Arg.isNull x)
+  testObject
+    (arbitraryObj $ ObjGenOpts ["foo", "bar", "bazz"] [] 0 3)
+    "Object schema with 3 properties and additional allowed"
+    "3-property-additional-allowed-1.medea"
+    (\x -> hasProperty "foo" Arg.isBoolean x && hasProperty "bazz" Arg.isString x)
+  testObject
+    (arbitraryObj $ ObjGenOpts ["bar", "bazz"] ["foo"] 0 3)
+    "Object schema with 3 properties and additional allowed"
+    "3-property-additional-allowed-2.medea"
+    (\x -> hasOptionalProperty "foo" Arg.isNumber x && hasProperty "bazz" Arg.isNull x)
+     -- These tests are for objects where additional are not allowed but are still found.
+  -- The generator is such that additional properties always exists.
+  testInvalidObject 
+    (arbitraryObj $ ObjGenOpts ["foo"] [] 1 3)
+    "Object schema with 1 property and no additional allowed"
+    "1-property-no-additional-1.medea"
+  testInvalidObject 
+    (arbitraryObj $ ObjGenOpts ["foo"] [] 1 3) 
+    "Object schema with 1 property and no additional allowed"
+    "1-property-no-additional-2.medea"
+  testInvalidObject 
+    (arbitraryObj $ ObjGenOpts [] ["foo"] 1 3)
+    "Object schema with 1 property and no additional allowed"
+     "1-property-no-additional-3.medea"
+  testInvalidObject 
+    (arbitraryObj $ ObjGenOpts ["foo", "bar", "bazz"] [] 1 3)
+    "Object schema with 3 properties and no additional allowed"
+    "3-property-no-additional-1.medea"
+  testInvalidObject 
+    (arbitraryObj $ ObjGenOpts ["bar", "bazz"] ["foo"] 1 3)
+    "Object schema with 3 properties and no additional allowed"
+     "3-property-no-additional-2.medea"
 
 
 -- helpers
+
+prependTestDir :: String -> String
+prependTestDir s = "./conformance/validation/" <> s
+
+  -- due to Mote and Spec preventing Effects from within a group/describe, 
+  -- we need to execute effects outside the group or inside the test only
+testWrap :: String -> String -> (Either LoaderError Schema -> TestPlanM Unit) -> (Schema -> TestPlanM Unit) -> TestPlanM Unit
+testWrap name fp eitherTests tests = do
+  result <- lift $ runTestM $ loadSchemaFromFile $ prependTestDir $ fp
+  group ("Test group: " <> name) $ do
+    eitherTests result
+    case result of 
+      Left _ -> test ("Not Left file: " <> fp) (fail "unexpected Left")
+      Right scm -> do
+         tests scm
+
+testWrap' :: String -> String -> (Schema -> TestPlanM Unit) -> TestPlanM Unit
+testWrap' name fp tests = testWrap name fp (const $ pure unit) tests
+
+testObject :: Gen Json -> String -> String -> (Json -> Boolean) -> TestPlanM Unit
+testObject gen name fp p = testWrap' name fp $ \scm -> do
+        test ("Should validate valid objects" <> ": " <> fp) (liftEffect $ yesProp gen p scm)
+        test ("Should not validate invalid objects" <> ": " <> fp) (liftEffect $ noProp gen (not <<< p) scm)
+
+-- Tests for Object values that should always get invalidated.
+testInvalidObject :: Gen Json -> String -> String -> TestPlanM Unit
+testInvalidObject gen name fp = testWrap' name fp $ \scm -> do
+  test ("Should not validate" <> ": " <> fp) (liftEffect $ noProp gen (const true) scm)
+
+
+-- "validation succeeded" property
+yesProp :: Gen Json -> (Json -> Boolean) -> Schema -> Effect Unit
+yesProp gen p scm = quickCheckGen $ do
+  v <- gen
+  pure $ prop v
+  where
+    prop v = toResult p v ==> toResult (isRight <<< runExcept <<< validate scm <<< Arg.stringify) v
+
+-- "validation failed" property
+noProp :: Gen Json -> (Json -> Boolean) -> Schema -> Effect Unit
+noProp gen p scm = quickCheckGen $ do 
+  v <- gen
+  pure $ prop v
+  where
+    prop v = toResult p v ==> toResult (isLeft <<< runExcept <<< validate scm <<< Arg.stringify) v
+
+-- Returns true iff the value is an object with the given property and the
+-- property-value satisfies the predicate.
+hasProperty :: String -> (Json -> Boolean) -> Json -> Boolean
+hasProperty propName p j 
+  = Arg.caseJsonObject 
+    false  
+    (\obj -> maybe false p $ Obj.lookup propName obj)
+    j
+
+-- Like hasProperty but is also true when the given property is absent.
+hasOptionalProperty :: String -> (Json -> Boolean) -> Json -> Boolean
+hasOptionalProperty propName p j
+  = Arg.caseJsonObject 
+    false
+    (\obj -> maybe true p $ Obj.lookup propName obj)
+    j
+
 isNullOr :: (Json -> Boolean) -> (Json -> Boolean)
 isNullOr f = (||) <$> Arg.isNull <*> f
 
 testAny :: String -> String -> TestPlanM Unit
-testAny fp  name = do
-  result <- lift $ runTestM $ loadSchemaFromFile fp
-  -- due to Mote and Spec preventing Effects from within a group/describe, 
-  -- we need to execute effects outside the group or inside the test only
-  group name $ do
-    test ("should parse: " <> fp) (result `shouldNotSatisfy` isParseError)
-    test ("should build: " <> fp) (result `shouldNotSatisfy` isSchemaError)
-    case result of 
-      Left _ -> test ("Not Left " <> name <> " file: " <> fp) (fail "unexpected Left")
-      Right scm -> do
+testAny fp name 
+  = testWrap name fp
+    (\result -> do
+      test ("should parse: " <> fp) (result `shouldNotSatisfy` isParseError)
+      test ("should build: " <> fp) (result `shouldNotSatisfy` isSchemaError)
+    )
+    (\scm -> do
          test ("should validate anything: " <> fp) ( liftEffect $ quickCheck (\rj -> (go scm $ rj) <?> "Test Failed for input" <> show rj))
+    )
   where
     go scm (RandomJson j) = isRight $ runExcept <<< validate scm <<< Arg.stringify $ j 
 
 testSingular :: String -> String -> (Json -> Boolean) -> TestPlanM Unit
-testSingular fp name p = do
-  result <- lift $ runTestM $ loadSchemaFromFile fp
-  group name $ do
+testSingular fp name p = testWrap name fp
+  (\result -> do
     test ("should parse: " <> fp) (result `shouldNotSatisfy` isParseError)
     test ("should build: " <> fp) (result `shouldNotSatisfy` isSchemaError)
-    case result of 
-      Left _ -> test ("Not Left " <> name <> " file: " <> fp) (fail "unexpected Left")
-      Right scm -> do
-        test ("should validate " <> name <> "s: " <> fp) (liftEffect $ quickCheck (\rj -> yesProp scm rj))
-        test ("should not validate non-" <> name <> "s: " <> fp) (liftEffect $ quickCheck (\rj -> noProp scm rj))
+  )
+  (\scm -> do
+    test ("should validate " <> name <> "s: " <> fp) (liftEffect $ quickCheck (\rj -> yesProp scm rj))
+    test ("should not validate non-" <> name <> "s: " <> fp) (liftEffect $ quickCheck (\rj -> noProp scm rj))
+  )
   where
     yesProp scm (RandomJson j) = toResult p j ==> toResult (isRight <<< runExcept <<< validate scm <<< Arg.stringify) j
     noProp scm (RandomJson j) = toResult (not <<< p) j ==> toResult (isLeft <<< runExcept <<< validate scm <<< Arg.stringify) j
 
 testStringVals :: String -> NonEmpty Array String -> TestPlanM Unit
-testStringVals fp validStrings= do
-  result <- lift $ runTestM $ loadSchemaFromFile fp
-  let name = "string is one of " <> show validStrings
-  case result of
-    Left _ ->test ("Not Left " <> name <> " file: " <> fp) (fail "unexpected left")
-    Right scm -> do
+testStringVals fp validStrings= 
+  let name = "string is one of " <> show validStrings in
+  testWrap' name fp
+    (\scm -> do
       test ("Should Validate " <> name <> "s: " <> fp) (liftEffect $ quickCheckGen $ validationIsCorrect $ scm)
       test ("Shouldn't Validate " <> name <> "s: " <> fp) (liftEffect $ quickCheckGen $ invalidationIsCorrect $ scm)
+    )
   where
     validationIsCorrect = validationTest identity isRight
     invalidationIsCorrect = validationTest not isLeft
